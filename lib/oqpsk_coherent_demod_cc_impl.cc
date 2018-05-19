187f4efe55bcf2146f41adae371ddca840e15449
bg2bhc <bg2bhc@gmail.com>
2018-05-19 23:45:24 +0800

    edit complex expressions

--- lib/oqpsk_coherent_demod_cc_impl.cc
+++ lib/oqpsk_coherent_demod_cc_impl.cc
@@ -168,8 +168,7 @@ namespace gr {
 		}
 		
 		gr_complex nco;
-		nco.real() = cos(-d_phase);
-		nco.imag() = sin(-d_phase);
+		nco = cos(-d_phase) + 1i*sin(-d_phase);
 
 		for(int j=0; j<(d_taps.size()-1); j++)
 		{
@@ -182,8 +181,7 @@ namespace gr {
 			d_mf_out[j] = d_mf_out[j+1];
 		}		
 
-		d_mf_out[d_samples_per_symbol/2].real() = 0.0f;
-		d_mf_out[d_samples_per_symbol/2].imag() = 0.0f;
+		d_mf_out[d_samples_per_symbol/2] = 0.0f;
 		for(int j=0; j<d_taps.size(); j++)
 		{
 			d_mf_out[d_samples_per_symbol/2] = d_mf_out[d_samples_per_symbol/2] + d_mix_out[j] * d_taps[j];
@@ -197,8 +195,7 @@ namespace gr {
 			//out[i_output].imag() = d_mf_out[d_samples_per_symbol/2].imag();
 
 			/* For opt_point=4, [B0, B1], [B2, B3]... */
-			out[i_output].real() = d_mf_out[0].imag();
-			out[i_output].imag() = d_mf_out[d_samples_per_symbol/2].real();
+			out[i_output] = d_mf_out[0].imag() + 1i*d_mf_out[d_samples_per_symbol/2].real();
 			i_output++;
 
 			/* For opt_point=12, [B-1, B0], [B1, B2]... */
